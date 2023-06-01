c492c641c5b3a1a715755b63691f8b0ab99383d6
shulman33 <samshulman6@gmail.com>
2023-06-01 10:04:56 -0400

    added delete functionality

--- cms/src/views/AppbarSection.vue
+++ cms/src/views/AppbarSection.vue
@@ -6,7 +6,7 @@
       :elevation="6"
       color="#F8F8FF"
     >
-      <v-form >
+      <v-form @submit.prevent>
         <v-text-field
           v-for="(title, index) in appbarTitles"
             :key="index"
@@ -19,25 +19,26 @@
     </v-sheet>
 
     <v-sheet 
-      class="mt-14 ml-16 content-width"
-      :elevation="6"
-      color="#F8F8FF"
-    >
-      <v-checkbox
-        v-for="(title, index) in appbarTitles"
-        :label="title"
-        color="#26547C"
-        :key="'checkbox-' + index"
-      ></v-checkbox>
-      <v-btn type="submit" color="#FF3333" block class="mt-2 custom-button">Delete</v-btn>
-    </v-sheet>
+    class="mt-14 ml-16 content-width"
+    :elevation="6"
+    color="#F8F8FF"
+  >
+    <v-checkbox
+      v-for="(title, index) in appbarTitleArray"
+      v-model="checkboxStates[index]"
+      :label="title"
+      color="#26547C"
+      :key="'checkbox-' + index"
+    ></v-checkbox>
+    <v-btn type="submit" @click="deleteTitles" color="#FF3333" block class="mt-2 custom-button">Delete</v-btn>
+  </v-sheet>
   </div>
   <v-sheet 
   class="mt-14"
       :elevation="6"
       color="#F8F8FF"
     >
-    <v-form >
+    <v-form @submit.prevent>
         <v-text-field
           v-model="addTitleValue"
           label="Add Title"
@@ -49,11 +50,11 @@
 
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { getSection, addSection, deleteSection } from '../API/endpoints'
 
 const appbarTitles = ref({});
-const newTitles = ref([])
+const checkboxStates = ref([]);
 const succesfullEdit = ref(false)
 const succesfullAdd = ref(false)
 const sucessfullDelete = ref(false)
@@ -79,19 +80,47 @@ const addTitle = async () => {
   };
 
   await addSection('homepageSections.appBar/titles', data);
+
+  // reload the data
+  const sectionData = await getSection('app-bar');
+  appbarTitles.value = sectionData.titles;
 }
 
+const deleteTitles = async () => {
+  // Iterate over the checkboxStates
+  for (const [index, isChecked] of checkboxStates.value.entries()) {
+    if (isChecked) {
+      const titleKey = appbarTitleKeys.value[index];
+      await deleteSection('homepageSections.appBar.titles/' + titleKey);
+    }
+  }
+
+  // reload the data
+  const sectionData = await getSection('app-bar');
+  appbarTitles.value = sectionData.titles;
+
+  // Reset the checkbox states
+  checkboxStates.value = Array(appbarTitleArray.value.length).fill(false);
+};
 
 const editTitles = async () => {
   await addSection('homepageSections.appBar/titles', appbarTitles.value)
+
+  // reload the data
+  const sectionData = await getSection('app-bar');
+  appbarTitles.value = sectionData.titles;
 }
 
 onMounted(async () => {
   const sectionData = await getSection('app-bar')
   appbarTitles.value = sectionData.titles
+  checkboxStates.value = Array(appbarTitleArray.value.length).fill(false);
   console.log("--------------------")
   console.log(JSON.stringify(appbarTitles.value))
 })
+
+const appbarTitleArray = computed(() => Object.values(appbarTitles.value));
+const appbarTitleKeys = computed(() => Object.keys(appbarTitles.value));
 
 
 </script>
